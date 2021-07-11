686025590806354946fabaf400cbc328ec59e484
shivaprakash-sudo <70264965+shivaprakash-sudo@users.noreply.github.com>
2021-07-11 21:03:53 +0100

    Add files via upload
    added all the files through upload

---
+++ sample_top_trumps_website/script.js
@@ -0,0 +1,15 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function openSignUpForm() {
+    document.getElementById("sign-up-form").style.display = "block";
+}
+
+function closeSignUpForm() {
+    document.getElementById("sign-up-form").style.display = "none";
+}
