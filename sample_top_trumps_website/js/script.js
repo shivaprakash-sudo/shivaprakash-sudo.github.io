a80fe6c1119581095107e8072b3a57ff64d1db73
Shiva Prakash Pendem <70264965+shivaprakash-sudo@users.noreply.github.com>
2021-07-21 01:14:29 +0100

    added top trumps website files

---
+++ sample_top_trumps_website/js/script.js
@@ -0,0 +1,21 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function openSignUpForm() {
+    document.getElementById("sign-up-form").style.display = "block";
+}
+
+function closeSignUpForm() {
+    document.getElementById("sign-up-form").style.display = "none";
+}
+
+// button actions
+
+function openProfileSettings() {
+
+}
