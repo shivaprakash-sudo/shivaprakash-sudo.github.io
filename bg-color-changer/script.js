4076c084d9abf8fadf74bd28e5c14c43cdd3072f
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2021-08-18 08:04:36 +0100

    added hex value to display and some styling

--- bg-color-changer/script.js
+++ bg-color-changer/script.js
@@ -1,11 +1,21 @@
 const body = document.body;
 const btn = document.querySelector(".btn");
+const hex_val = document.querySelector("#hex-val")
+
+const symbols = "0123456789ABCDEF";
+const symbolsArray = symbols.split("");
 
 btn.addEventListener("click", (e) => {
     body.style.backgroundColor = colorGenerator();
+    hex_val.textContent = `${colorGenerator()}`
 })
 
 function colorGenerator() {
-    let hex = `#${Math.floor(Math.random()*1000000)}`;
+    let hex = "#";
+    const hexLength = 6;
+    for (let index = 0; index < hexLength; index++) {
+        const index = Math.floor(Math.random() * 16)
+        hex += symbolsArray[index];
+    }
     return hex;
 }
